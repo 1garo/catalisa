8f39c0c153ef760682e2856245419cb2615d7219
1garo <alevardai427@gmail.com>
2023-12-11 13:16:55 -0300

    feat: add utils folder

---
+++ src/utils/utils.ts
@@ -0,0 +1,36 @@
+import { Account } from "@prisma/client";
+import { PrismaService } from "../service/prisma.service";
+
+export const findAccount = async (prisma: PrismaService, id: string): Promise<Account | null> => {
+    const account = await prisma.account.findUnique({
+      where: {
+        id: Number(id),
+        deletedAt: null,
+      },
+    });
+
+    if (account === null) {
+      return null
+    }
+
+    return account
+}
+
+export const findAccountByNumber = async (prisma: PrismaService, number: number): Promise<Account | null> => {
+  if (number === null) {
+    return null
+  }
+
+  const account = await prisma.account.findUnique({
+    where: {
+      number,
+      deletedAt: null
+    },
+  });
+
+  if (account === null) {
+    return null
+  }
+
+  return account
+}
